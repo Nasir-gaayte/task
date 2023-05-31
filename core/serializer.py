075c6ef9af66d18af25adda5e829a32e476a6ad9
Nasir-gaayte <slowboyz12@gmail.com>
2023-05-31 12:32:16 +0300

    fix category and city get the name not id in contact

--- core/serializer.py
+++ core/serializer.py
@@ -10,11 +10,20 @@ class ContactSerializer(ModelSerializer):
     class Meta:
         model = Contactmodel
         fields = '__all__'
+    
+    def to_representation(self, instance):
+        rep= super(ContactSerializer,self).to_representation(instance)  
+        rep['city'] = instance.city.name 
+        rep['category']= instance.category.name 
+        return rep
+    
+        
 
 class CitySerializer(ModelSerializer):
     class Meta:
         model = Citymodel
         fields ='__all__'
+   
         
 class CategorySerializer(ModelSerializer):
     class Meta:
